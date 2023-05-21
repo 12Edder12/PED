38379cb7a288421edeb16f85bcc97a5e498554e9
Matias Tite <matiastite1@gmail.com>
2023-05-21 11:21:17 -0500

    Esto es una prueba del proceso que explique en wssp

--- Tests/MainTests.java
+++ Tests/MainTests.java
@@ -6,5 +6,6 @@ public class MainTests {
     public static void main(String[] args) {
         System.out.println("ESto es un mensaje de prueba que pidio don Eddel");
         System.out.println("Una plueba mas uwu");
+        System.out.println("Esto es una prueba del proceso que les enseñe mmvrgs");
     }
 }
