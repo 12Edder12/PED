d57de03c98e8263683f1fd84d9afdb83ac475d30
Matias Tite <matiastite1@gmail.com>
2023-05-20 22:31:49 -0500

    Update main.java

--- main.java
+++ main.java
@@ -1,5 +1,8 @@
-public class main{
+public class main {
     public static void main(String[] args) {
-    System.out.println("Welcome");
+        System.out.println("Welcome");
+        String soyGay = "El henry es muy gay porque no le sabe al git";
+        System.out.println("Edder eres mi dios");
+        System.out.println(soyGay);
     }
 }
